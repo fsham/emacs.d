d21868f41bf35964941086f66cfb172ed95df1f1
fedor.shamakhov@gmail.com <fedor.shamakhov@gmail.com>
2017-11-25 15:25:11 +0800

    commented counsel, swiper, treemacs. modified magit settings

--- elisp/core.el
+++ elisp/core.el
@@ -27,50 +27,52 @@
          ("C-c ]" . bm-next)))
 
 
-(use-package counsel
-  :ensure t
-  :bind
-  (("M-x" . counsel-M-x)
-   ("M-y" . counsel-yank-pop)
-   :map ivy-minibuffer-map
-   ("M-y" . ivy-next-line)))
+;; (use-package counsel
+;;   :ensure t
+;;   :bind
+;;   (("M-x" . counsel-M-x)
+;;    ("M-y" . counsel-yank-pop)
+;;    :map ivy-minibuffer-map
+;;    ("M-y" . ivy-next-line)))
 
-(use-package swiper
-  :pin melpa-stable
-  :diminish ivy-mode
-  :ensure t
-  :bind*
-  (("C-s" . swiper)
-   ("C-c C-r" . ivy-resume)
-   ("C-x C-f" . counsel-find-file)
-   ("C-c h f" . counsel-describe-function)
-   ("C-c h v" . counsel-describe-variable)
-   ("C-c i u" . counsel-unicode-char)
-   ("M-i" . counsel-imenu)
-   ("C-c g" . counsel-git)
-   ("C-c j" . counsel-git-grep)
-   ("C-c k" . counsel-ag)
-   ("C-c l" . scounsel-locate))
-  :config
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (define-key read-expression-map (kbd "C-r") #'counsel-expression-history)
-    (ivy-set-actions
-     'counsel-find-file
-     '(("d" (lambda (x) (delete-file (expand-file-name x)))
-        "delete"
-        )))
-    (ivy-set-actions
-     'ivy-switch-buffer
-     '(("k"
-        (lambda (x)
-          (kill-buffer x)
-          (ivy--reset-state ivy-last))
-        "kill")
-       ("j"
-        ivy--switch-buffer-other-window-action
-        "other window")))))
+;; (use-package swiper
+;;   :pin melpa-stable
+;;   :diminish ivy-mode
+;;   :ensure t
+;;   :bind*
+;;   (("C-s" . swiper)
+;;    ("C-c C-r" . ivy-resume)
+;;    ("C-x C-f" . counsel-find-file)
+;;    ("C-c h f" . counsel-describe-function)
+;;    ("C-c h v" . counsel-describe-variable)
+;;    ("C-c i u" . counsel-unicode-char)
+;;    ("M-i" . counsel-imenu)
+;;    ("C-c g" . counsel-git)
+;;    ("C-c j" . counsel-git-grep)
+;;    ("C-c k" . counsel-ag)
+;;    ("C-c l" . scounsel-locate))
+;;   :config
+;;   (progn
+;;     (ivy-mode 1)
+;;     (setq ivy-use-virtual-buffers t)
+;;     (setq ivy-use-selectable-prompt t)
+;;     (setq enable-recursive-minibuffers t)
+;;     (define-key read-expression-map (kbd "C-r") #'counsel-expression-history)
+;;     (ivy-set-actions
+;;      'counsel-find-file
+;;      '(("d" (lambda (x) (delete-file (expand-file-name x)))
+;;         "delete"
+;;         )))
+;;     (ivy-set-actions
+;;      'ivy-switch-buffer
+;;      '(("k"
+;;         (lambda (x)
+;;           (kill-buffer x)
+;;           (ivy--reset-state ivy-last))
+;;         "kill")
+;;        ("j"
+;;         ivy--switch-buffer-other-window-action
+;;         "other window")))))
 
 
 ;; (use-package ace-window
@@ -97,10 +99,12 @@
 (use-package magit
   :init
   (global-magit-file-mode 1)
+  (add-hook 'magit-mode-hook 'turn-on-magit-gitflow)
   :bind
-  (("C-c m" . magit-status)
+  (("C-x g" . magit-status)
    ("C-x M-g" . magit-dispatch-popup))
   )
+(use-package magit-gitflow)
 
 (setq inhibit-startup-message t)
 ;; (global-linum-mode)
@@ -120,54 +124,54 @@
 
 (electric-pair-mode t)
 
-(use-package dash-functional)
-(use-package all-the-icons)
-(use-package treemacs
-  :ensure t
-  :defer t
-  :config
-  (progn
-    (use-package treemacs-evil
-      :ensure t
-      :demand t)
-    (setq treemacs-follow-after-init          t
-          treemacs-width                      50
-          treemacs-indentation                2
-          treemacs-git-integration            t
-          treemacs-collapse-dirs              3
-          treemacs-silent-refresh             nil
-          treemacs-change-root-without-asking nil
-          treemacs-sorting                    'alphabetic-desc
-          treemacs-show-hidden-files          t
-          treemacs-never-persist              nil
-          treemacs-is-never-other-window      nil
-          treemacs-goto-tag-strategy          'refetch-index)
+;; (use-package dash-functional)
+;; (use-package all-the-icons)
+;; (use-package treemacs
+;;   :ensure t
+;;   :defer t
+;;   :config
+;;   (progn
+;;     (use-package treemacs-evil
+;;       :ensure t
+;;       :demand t)
+;;     (setq treemacs-follow-after-init          t
+;;           treemacs-width                      50
+;;           treemacs-indentation                2
+;;           treemacs-git-integration            t
+;;           treemacs-collapse-dirs              3
+;;           treemacs-silent-refresh             nil
+;;           treemacs-change-root-without-asking nil
+;;           treemacs-sorting                    'alphabetic-desc
+;;           treemacs-show-hidden-files          t
+;;           treemacs-never-persist              nil
+;;           treemacs-is-never-other-window      nil
+;;           treemacs-goto-tag-strategy          'refetch-index)
 
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)))
+;;     (treemacs-follow-mode t)
+;;     (treemacs-filewatch-mode t)))
 
-(use-package treemacs-projectile
-  :defer t
-  :ensure t
-  :config
-  (setq treemacs-header-function #'treemacs-projectile-create-header))
+;; (use-package treemacs-projectile
+;;   :defer t
+;;   :ensure t
+;;   :config
+;;   (setq treemacs-header-function #'treemacs-projectile-create-header))
 
-(global-set-key (kbd "C-x C-e") 'treemacs-projectile)
-(global-set-key (kbd "C-x C-q") 'treemacs-projectile-toggle)
+;; ;; (global-set-key (kbd "C-x C-e") 'treemacs-projectile)
+;; (global-set-key (kbd "C-x C-q") 'treemacs-projectile-toggle)
 
-(with-eval-after-load "treemacs"
-  (defvar treemacs-custom-html-icon (all-the-icons-icon-for-file "name.html"))
-  (treemacs-define-custom-icon treemacs-custom-html-icon "html" "htm"))
+;; (with-eval-after-load "treemacs"
+;;   (defvar treemacs-custom-html-icon (all-the-icons-icon-for-file "name.html"))
+;;   (treemacs-define-custom-icon treemacs-custom-html-icon "html" "htm"))
 
-(with-eval-after-load "treemacs"
-  (defvar treemacs-custom-php-icon (all-the-icons-icon-for-file "name.php"))
-  (treemacs-define-custom-icon treemacs-custom-php-icon "php" "php"))
+;; (with-eval-after-load "treemacs"
+;;   (defvar treemacs-custom-php-icon (all-the-icons-icon-for-file "name.php"))
+;;   (treemacs-define-custom-icon treemacs-custom-php-icon "php" "php"))
 
-(with-eval-after-load "treemacs"
-  (defvar treemacs-custom-js-icon (all-the-icons-icon-for-file "name.js"))
-  (treemacs-define-custom-icon treemacs-custom-js-icon "js" "js")
-  (treemacs-define-custom-icon treemacs-custom-js-icon "jsx" "jsx")
-  )
+;; (with-eval-after-load "treemacs"
+;;   (defvar treemacs-custom-js-icon (all-the-icons-icon-for-file "name.js"))
+;;   (treemacs-define-custom-icon treemacs-custom-js-icon "js" "js")
+;;   (treemacs-define-custom-icon treemacs-custom-js-icon "jsx" "jsx")
+;;   )
 
 ;; git clone https://github.com/sebastiencs/sidebar.el.git
 ;; (use-package ov)
