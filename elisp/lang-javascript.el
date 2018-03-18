0641889b536e1293f990bc38ea14c3e07ec7a1a7
fedor.shamakhov@gmail.com <fedor.shamakhov@gmail.com>
2018-03-18 13:08:56 +0800

    javascript: import xref-js2

--- elisp/lang-javascript.el
+++ elisp/lang-javascript.el
@@ -1,14 +1,18 @@
 ;; js2-mode
 ;; https://github.com/mooz/js2-mode
 (use-package js2-mode
+  :defer t
   :bind (:map js2-mode-map
               (("C-x C-e" . js-send-last-sexp)
                ("C-M-x" . js-send-last-sexp-and-go)
                ("C-c C-b" . js-send-buffer-and-go)
-               ("C-c C-l" . js-load-file-and-go)))
+               ("C-c C-l" . js-load-file-and-go)
+               ("M-." . nil)
+               ))
   :mode
   ("\\.js$" . js2-mode)
   ("\\.json$" . js2-jsx-mode)
+  ("\\.jsx$" . js2-jsx-mode)
   :config
   (custom-set-variables '(js2-strict-inconsistent-return-warning nil))
   (custom-set-variables '(js2-strict-missing-semi-warning nil))
@@ -17,6 +21,13 @@
   (setq js2-indent-level 4)
   (setq js2-basic-offset 4)
   (setq sgml-basic-offset 4)
+  (use-package js2-refactor :defer t
+    :diminish js2-refactor-mode
+    :config
+    (js2r-add-keybindings-with-prefix "C-c j r"))
+  (add-hook 'js2-mode-hook 'js2-refactor-mode)
+  (add-hook 'js2-mode-hook (lambda ()
+                             (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
 
   ;; tern :- IDE like features for javascript and completion
   ;; http://ternjs.net/doc/manual.html#emacs
@@ -43,26 +54,21 @@
 
   ;; js2-refactor :- refactoring options for emacs
   ;; https://github.com/magnars/js2-refactor.el
-  (use-package js2-refactor :defer t
-    :diminish js2-refactor-mode
-    :config
-    (js2r-add-keybindings-with-prefix "C-c j r"))
-  (add-hook 'js2-mode-hook 'js2-refactor-mode)
-  (use-package ac-js2 :defer t)
-  (add-hook 'js2-mode-hook 'ac-js2-mode)
   )
 
+;; (use-package ac-js2 :defer t)
+;; (add-hook 'js2-mode-hook 'ac-js2-mode)
+(use-package js-import)
+(use-package xref-js2
+  :defer t
+  :config
+  (add-to-list 'xref-js2-ignored-dirs "public")
+  )
 (use-package rjsx-mode
   :mode
   ("\\.jsx$" . rjsx-mode)
-  :config
-  (setq indent-level 4)
-  (setq js-indent-level 4)
-  (setq js2-indent-level 4)
-  (setq js2-basic-offset 4)
-  (setq sgml-basic-offset 4)
-  (use-package ac-js2 :defer t)
-  (add-hook 'rjsx-mode-hook 'ac-js2-mode)
+  ;; (use-package ac-js2 :defer t)
+  ;; (add-hook 'rjsx-mode-hook 'ac-js2-mode)
   )
 
 (use-package typescript-mode :defer t)
