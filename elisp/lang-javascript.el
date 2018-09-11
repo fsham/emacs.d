c160ab64dc3d96873809d34c8303c783910e07a3
fedor.shamakhov@gmail.com <electron160zeptocoulomb@gmail.com>
2018-09-11 18:31:14 +0800

    tide

--- elisp/lang-javascript.el
+++ elisp/lang-javascript.el
@@ -1,5 +1,17 @@
 ;; js2-mode
 ;; https://github.com/mooz/js2-mode
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
 (use-package js2-mode
   :defer t
   :bind (:map js2-mode-map
@@ -27,6 +39,8 @@
     (js2r-add-keybindings-with-prefix "C-c j r"))
   (add-hook 'js2-mode-hook 'js2-refactor-mode)
 
+  (use-package ag)
+
   (use-package xref-js2
     :defer t
     :config
@@ -34,6 +48,16 @@
   (add-hook 'js2-mode-hook (lambda ()
                              (flycheck-mode 0)
                              (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
+
+  ;; (use-package tide
+  ;;   :ensure t
+  ;;   :after (typescript-mode company flycheck)
+  ;;   :hook ((typescript-mode . tide-setup)
+  ;;          (typescript-mode . tide-hl-identifier-mode)
+  ;;          (before-save . tide-format-before-save)))
+
+  ;; (add-hook 'js2-mode-hook #'setup-tide-mode)
+  ;; configure javascript-tide checker to run after your default javascript checker
 
   ;; (require 'tern)
   ;; (require 'tern-auto-complete)
@@ -49,7 +73,7 @@
   ;; Disable completion keybindings, as we use xref-js2 instead
   ;; (define-key tern-mode-keymap (kbd "M-.") nil)
   ;; (define-key tern-mode-keymap (kbd "M-,") nil)
-  (define-key js2-mode-map (kbd "M-.") nil)
+  ;; (define-key js2-mode-map (kbd "M-.") nil)
   (use-package flymake-jslint :defer t)
   ;; (add-hook 'js2-mode-hook 'flymake-jslint-load)
 
